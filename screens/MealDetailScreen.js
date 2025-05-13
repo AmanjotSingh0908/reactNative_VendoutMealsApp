2d805d4047924e7fca5beeb583260feb201d5407
AmanjotSingh0908 <amanjotsingh0908@gmail.com>
2025-05-14 01:14:51 +0530

    ft. styled Meal Page, created components for list and subtitle || pending scrollable and width fix

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,22 +1,52 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
-function MealDetailScreen({route}) {
+function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  return <View>
-    <Image source={{uri: selectedMeal.imageUrl}} />
-    <Text>{selectedMeal.title}</Text>
-    <MealDetails duration={selectedMeal.duration} complexity={selectedMeal.complexity} affordability={selectedMeal.affordability}/>
-    <Text>Ingredients</Text>
-    {selectedMeal.ingredients.map((ingredient) => <Text key={ingredient}>{ingredient}</Text>)}
-    <Text>Steps</Text>
-    {selectedMeal.ingredients.map((steps) => <Text key={steps}>{steps}</Text>)}
-
-  </View>
+  return (
+    <View>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <Subtitle>Ingredients</Subtitle>
+      <List data={selectedMeal.ingredients} />
+      <Subtitle>Steps</Subtitle>
+      
+      {/* {selectedMeal.ingredients.map((steps) => (
+        <Text key={steps}>{steps}</Text>
+      ))} =? Created a component for this*/}
+      <List data={selectedMeal.steps} />
+    </View>
+  );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  
+});
