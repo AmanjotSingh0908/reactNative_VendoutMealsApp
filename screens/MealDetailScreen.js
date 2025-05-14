c6d4d9e315d0cab122c66edfe39bb5365a553bf8
AmanjotSingh0908 <amanjotsingh0908@gmail.com>
2025-05-15 00:12:56 +0530

    ft. Tweaky tiny styles added

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, Image, StyleSheet } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
@@ -10,7 +10,7 @@ function MealDetailScreen({ route }) {
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View>
+    <ScrollView style={styles.rootCointainer}>
       <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
       <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
@@ -19,21 +19,28 @@ function MealDetailScreen({ route }) {
         affordability={selectedMeal.affordability}
         textStyle={styles.detailText}
       />
-      <Subtitle>Ingredients</Subtitle>
-      <List data={selectedMeal.ingredients} />
-      <Subtitle>Steps</Subtitle>
-      
-      {/* {selectedMeal.ingredients.map((steps) => (
+      <View>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+
+          {/* {selectedMeal.ingredients.map((steps) => (
         <Text key={steps}>{steps}</Text>
       ))} =? Created a component for this*/}
-      <List data={selectedMeal.steps} />
-    </View>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
+  rootCointainer: {
+    marginBottom: 32,
+  },
   image: {
     width: "100%",
     height: 350,
@@ -48,5 +55,10 @@ const styles = StyleSheet.create({
   detailText: {
     color: "white",
   },
-  
+  listOutputContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
 });
